cf56e15c2fd9c29b524c01abab5a113695b736f7
Brady Anderson <bradya48@gmail.com>
2019-03-23 16:53:24 -0500

    getters and setters for class and extra comments

--- Assignments/Program2/IndividualScripts/BradyVigenere.py
+++ Assignments/Program2/IndividualScripts/BradyVigenere.py
@@ -1,40 +1,45 @@
 import sys
 
-class VigenereCipher:
+class VigenereCipher(object):
+  # constructor for Vigenere Cipher
   def __init__(self, process, key):
-    self.key = key.lower()
+    self.process = process
+    self.key = key
     self.key_position = 0
-    if process == '-e':
-      self.process = "encrypt"
-    elif process == '-d':
-      self.process = "decrypt"
-    else:
-      raise Exception("Invalid process argument: " + process  + ". Use -e for encryption, -d for decryption")
     self.alpha, self.alpha_inverse = self.build_alpha_dicts()
 
   # encodes or decodes string based on state of cipher
   def cipher(self, s):
+    # separate all characters in string to list
     char_list = list(s)
+    # create a new list using char_list, call cipher_char() on each character
     cipher_list = [self.cipher_char(c) for c in char_list]
     self.reset_key_postion()
+    # take the list and merge all the characters into a string
     return ''.join(cipher_list)
 
   # encodes or decodes character based on state of cipher
   def cipher_char(self, c):
+    # if c isn't in alpha (dictionary built in constructor), just return c
+    # this is for catching characters like '!' or '#' which are not ciphered
     if c.lower() not in self.alpha:
       return c
+    # get current character of key for encoding
     key_char = self.get_current_key_char()
+    # save case of c to reference latter
     char_is_upper = c.isupper()
+    # preform vigenere cipher encode based on if cipher is set to encrypt or decrypt
     if self.process == "encrypt":
       cipher_val = self.alpha_inverse[(self.alpha[c.lower()] + self.alpha[key_char]) % 26]
     elif self.process == "decrypt":
       cipher_val = self.alpha_inverse[(26 + self.alpha[c.lower()] - self.alpha[key_char]) % 26]
-      
+    # return the ciphered value with correct case (upper or lower)
     return cipher_val.upper() if char_is_upper else cipher_val
 
   # encodes or decodes character based on state of cipher
   def get_current_key_char(self):
     key_char = self.key[self.key_position % len(self.key)]
+    # increment key to next position for next access
     self.key_position += 1
     return key_char
 
@@ -50,6 +55,34 @@ class VigenereCipher:
       alpha[chr(i + 97)] = i
       alpha_inverse[i] = chr(i + 97)
     return (alpha, alpha_inverse)
+
+  # All functions below are getters and setters class variables
+  @property
+  def key(self):
+    return self._key
+
+  @key.setter
+  def key(self, value):
+    new_value = value.replace(' ', '').lower()
+    if not new_value.isalpha():
+      print("Key must only contain alpha values")
+      exit()
+    self._key = new_value
+
+  @property
+  def process(self):
+    return self._process
+
+  @process.setter
+  def process(self, value):
+    if value == '-e':
+      process = "encrypt"
+    elif value == '-d':
+      process = "decrypt"
+    else:
+      print("Invalid process argument: " + value  + ". Use -e for encryption, -d for decryption")
+      exit()
+    self._process = process
 
 
 # Main
