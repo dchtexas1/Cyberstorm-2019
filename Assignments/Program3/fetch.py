1df2ff22f9f93f17a9b9ba5bf8adf95507c9b021
Brady Anderson <bradya48@gmail.com>
2019-04-02 16:32:36 -0500

    add program3 materials

---
+++ Assignments/Program3/fetch.py
@@ -0,0 +1,15 @@
+##########################################################################
+# GitHub Repo Link: https://github.com/dchtexas1/Cyberstorm-2019
+# GitHub Assignment Link: https://github.com/dchtexas1/Cyberstorm-2019/tree/master/Assignments/Program3
+#
+# CSC 442
+# Date: 04/02/19
+# Team Name: Romans
+# Names: Brady Anderson, Sam Dominguez, Dax Henson, Michael McCrary,
+#        Daniel Munger, Stephanie Niemiec, Holland Wolf
+# 
+# Description: 
+#
+# Run Instructions: python fetch.py
+#
+#######################################################################
