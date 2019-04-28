aacd35352c436e76b6d755158911db8f53f531db
Brady Anderson <bradya48@gmail.com>
2019-04-28 13:45:32 -0500

    fix for ciphertext2

--- Assignments/Program6/IndividualWork/brady_xor.py
+++ Assignments/Program6/IndividualWork/brady_xor.py
@@ -15,39 +15,11 @@
 ##############################################################################
 import binascii, string, sys
 
-class BinaryDecoder(object):
-    def decode(self, b_str, ascii_length):
-        """Return ASCII value of binary string"""
-        b_str = str(b_str).strip()
-        # remove extra bits if not properly divisible
-        b_str = b_str[0:len(b_str) - len(b_str) % ascii_length]
-        if not self.check_string(b_str):
-            raise ValueError("Binary string must only consist of 0's or 1's")
-        return self.__decode_chars(b_str, ascii_length)
-
-    def check_string(self, string):
-        """Return True or False if proper binary string"""
-        return set(string).issubset(['0', '1'])
-
-    def binary_to_dec(self, n):
-        """Convert binary string to integer"""
-        return int(n, 2)
-
-    def __decode_chars(self, b_str, ascii_length):
-        """Decode binary string based of specified bit length"""
-        decoded_chars = []
-        temp_str = b_str
-        print(len(b_str))
-        print(ascii_length)
-        while len(temp_str) > 0:
-            to_decode = temp_str[0:ascii_length]
-            temp_str = temp_str[ascii_length:]
-            decode_dec = self.binary_to_dec(to_decode)
-            decoded_chars.append(chr(decode_dec))
-        return "".join(decoded_chars)
-
 def ascii_to_binary(a_string):
   return bin(int(binascii.hexlify(a_string), 16))[2:].zfill(len(a_string) * 8)
+
+def binary_to_ascii(b_string):
+  return binascii.unhexlify('%x' % int(b_string, 2))
 
 def match_length(s, length):
   """Extend or tuncate string to match given length"""
@@ -65,16 +37,27 @@ def match_length(s, length):
 def xor(b_str_1, b_str_2):
   return bin(int(b_str_1, 2) ^ int(b_str_2, 2))[2:].zfill(len(b_str_1))
 
+def file_to_binary(f):
+  b = ''
+  while True:
+    try:
+      b += bin(int(binascii.hexlify(f.read(1)), 16))[2:].zfill(8)
+    except:
+      break
+  return b
 
 # ---- Main ----
-key_f = open('key', 'rb').read()
-key_bin = ascii_to_binary(key_f)
-bd = BinaryDecoder()
+# key_f = open('key', 'rb').read()
+key_f = open('key', 'rb')
+# key_bin = ascii_to_binary(key_f)
 
+key_bin = file_to_binary(key_f)
+# print(len(key_bin))
+  
 file_bin = ""
 for line in sys.stdin:
   file_bin += ascii_to_binary(line)
 modified_key_bin = match_length(key_bin, len(file_bin))
 xored_bin = xor(file_bin, modified_key_bin)
-xor_ascii = bd.decode(xored_bin, 8)
+xor_ascii = binary_to_ascii(xored_bin)
 sys.stdout.write(xor_ascii)
